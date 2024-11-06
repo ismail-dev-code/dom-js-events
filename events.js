0b73eed1d1a68cc7cf69bfe266a500434dc69fcd
ismail-dev-code <ismail.online0385678@gmail.com>
2024-11-06 17:16:13 +0600

    github delete added

--- events.js
+++ events.js
@@ -1,39 +1,49 @@
-// option one directly set in the html element 
+// // option one directly set in the html element 
 
-// option two 
-function makeRed(){
-    document.body.style.backgroundColor="red";
-}
+// // option two 
+// function makeRed(){
+//     document.body.style.backgroundColor="red";
+// }
  
-// option three 
-const makeBlueButton = document.getElementById('make-blue');
-        makeBlueButton.onclick = makeBlue;
+// // option three 
+// const makeBlueButton = document.getElementById('make-blue');
+//         makeBlueButton.onclick = makeBlue;
      
-        function makeBlue(){
-            document.body.style.backgroundColor= 'blue';
-        }
+//         function makeBlue(){
+//             document.body.style.backgroundColor= 'blue';
+//         }
 
-        // option three another 
-        const makeSkyBlue = document.getElementById('make-skyblue');
-        makeSkyBlue.onclick = function makeskyblue(){
-            document.body.style.backgroundColor= 'skyblue';
-        }
+//         // option three another 
+//         const makeSkyBlue = document.getElementById('make-skyblue');
+//         makeSkyBlue.onclick = function makeskyblue(){
+//             document.body.style.backgroundColor= 'skyblue';
+//         }
 
-        // Option four 
-        const pinkButton = document.getElementById('make-pink');
-     pinkButton.addEventListener('click', makePink);
+//         // Option four 
+//         const pinkButton = document.getElementById('make-pink');
+//      pinkButton.addEventListener('click', makePink);
 
-     function makePink(){
-        document.body.style.backgroundColor='pink';
-     }
-    //  option four another 
-    const makeGreenButton = document.getElementById('make-green');
-    makeGreenButton.addEventListener('click', function makeGreen(){
-        document.body.style.backgroundColor='green';
-    }) 
+//      function makePink(){
+//         document.body.style.backgroundColor='pink';
+//      }
+//     //  option four another 
+//     const makeGreenButton = document.getElementById('make-green');
+//     makeGreenButton.addEventListener('click', function makeGreen(){
+//         document.body.style.backgroundColor='green';
+//     }) 
 
-    // option four final 
-    // important : we will use this sometimes 
-    document.getElementById('make-orange').addEventListener('click', function(){
-        document.body.style.backgroundColor= 'orange';
-    }); 
+//     // option four final 
+//     // important : we will use this sometimes 
+//     document.getElementById('make-orange').addEventListener('click', function(){
+//         document.body.style.backgroundColor= 'orange';
+//     }); 
+
+
+
+    function sum (num1, num2){
+        const plus = num1+num2;
+        return plus 
+    }
+
+    const result = sum(25,347)
+    console.log(result);
